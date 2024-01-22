cc61517c230b0c9d413827fff87ec6658f2aa3b7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-01-22 11:12:08 -0500

    more comments

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,3 +3,18 @@
 /// <reference types="cypress" />
 // intellisense for "cypress-if" commands
 /// <reference types="cypress-if" />
+
+// TODO: modify this test to use "cypress-if" commands
+// and only expand the section
+// only if its attribute "aria-expanded" is "false"
+// Tip: you can change the initial section by editing the "index.html" file
+it('expands the element if needed', () => {
+  cy.visit('index.html')
+  cy.contains('h1', 'Test page').should('be.visible')
+  // we don't know if the section is expanded or not
+  // find the "Expand" button and click it
+  // but only if the section is not expanded already
+  cy.contains('button', 'Expand').click()
+  // the section should be visible now
+  cy.get('section').should('be.visible')
+})
